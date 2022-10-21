75b22aceb8587e4056b2625f0ad93c59e99e31df
Polak <somsiad@gmail.com>
2022-10-21 09:43:50 +0200

    update z lekcji

--- Zadanie 2.sql
+++ Zadanie 2.sql
@@ -5,3 +5,12 @@ SELECT ac.actor_id, ac.first_name, ac.last_name, fl.title FROM
 actor AS ac INNER JOIN film_actor AS fla USING (actor_id)
 INNER JOIN film AS fl USING (film_id)
 WHERE fl.title = 'AFFAIR PREJUDICE';
+
+
+-- | actor_id | first_name | last_name | title |
+
+-- | 41 | JODIE | DEGENERES | AFFAIR PREJUDICE |
+-- | 81 | SCARLETT | DAMON | AFFAIR PREJUDICE |
+-- | 88 | KENNETH | PESCI | AFFAIR PREJUDICE |
+-- | 147 | FAY | WINSLET | AFFAIR PREJUDICE |
+-- | 162 | OPRAH | KILMER | AFFAIR PREJUDICE |
