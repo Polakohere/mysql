c6887403a2ec6fbc924652f06c3da57e76768865
Polak <somsiad@gmail.com>
2022-10-21 08:49:56 +0200

    Update

--- Zapytania-21,10,22.sql
+++ Zapytania-21,10,22.sql
@@ -1,2 +1,7 @@
 USE sakila;
+
+-- Column Aliases
+-- Column aliases are useful for improving the expression of your queries, reducing the
+-- number of characters you need to type, and making it easier to work with program‐
+-- ming languages such as Python or PHP. Consider a simple, not-very-useful example:
 SELECT first_name AS 'First Name', last_name AS 'Last Name' FROM actor LIMIT 5; 
